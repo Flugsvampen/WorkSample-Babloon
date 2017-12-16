0a712cc6cc746da0468aa51e9a88a97dfbc396a0
Dennis Karlsson <dennis.k9705@gmail.com>
2017-12-16 17:52:55 +0100

    - Initial commit - Added third-person movement and camera controller

---
+++ Work sample - programmer for Babloon Studios AB/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float rotateSpeed = 20f;
+
+    private Transform target;
+
+    
+    void Start()
+    {
+        target = transform.parent;
+    }
+
+    
+    void Update()
+    {
+        float rotateDegreesX = 0f;
+        
+        rotateDegreesX += Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+
+        transform.RotateAround(target.position, Vector3.up, rotateDegreesX);
+    }
+}
---
+++ Work sample - programmer for Babloon Studios AB/Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+
+    private Transform cameraTrans;
+    private Vector3 cameraDirFwd;
+    private Vector3 cameraDirSide;
+    private float moveFwd;
+    private float moveSide;
+
+
+    void Start()
+    {
+        cameraTrans = transform.GetChild(0);
+    }
+
+
+    void Update()
+    {
+        // Gets direction of camera
+        cameraDirFwd = transform.position - cameraTrans.position;
+        cameraDirFwd.y = 0;
+        cameraDirFwd.Normalize();
+
+        // Gets sideways direction of camera
+        cameraDirSide = Quaternion.Euler(0f, 90f, 0f) * cameraDirFwd;
+
+        moveFwd = Input.GetAxis("Vertical");
+        moveSide = Input.GetAxis("Horizontal");
+        
+        transform.Translate((cameraDirFwd * moveFwd + cameraDirSide * moveSide) * moveSpeed * Time.deltaTime);
+    }
+}
